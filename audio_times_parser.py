79ebb73f0f75c527d2e26abef38128bbb8c619af
Andy Ortiz <a.ortizg@uniandes.edu.co>
2021-12-01 03:00:47 +0100

    Fixes parser and generates dictionary with track info. Adds preliminary script to automate ffmpeg instructions that split a larger audio track and merges it with a rain background.

---
+++ audio_times_parser.py
@@ -0,0 +1,67 @@
+import subprocess
+import os
+
+# constants 
+SOURCE_PATH = '/Users/renegade/MyRepos/automated-audio-splitter/in'
+OUT_PATH = '/Users/renegade/MyRepos/automated-audio-splitter/out'
+ORIGIN_TRACK = 'bdsp.flac'  # TODO: set this as path var
+
+
+def parseTrackList():
+    with open(f'{SOURCE_PATH}/audio_times.txt', 'r') as f:
+        tracks = {}
+
+        # create dictionary of tracks
+        prev = ""
+        curr = f.readline()
+
+        # by reading the 1st line outside the loop, we can get all n and n+1 lines 
+        # (except for the final one since we don't know the end time. This will be handled later.
+        for line in f:
+            # assign prev to the current line
+            prev = curr
+
+            # assign curr to the next line 
+            curr = line
+
+            # obtain information about the track
+            substr = prev.strip('\n ', )
+            track_id = substr.partition('.')[0]
+            track_name = (substr[substr.find('.') + 1:substr.find(':')]).replace(" ", "_")
+            start_time = substr.partition(':')[2]
+
+            # obtain end time of the previous track by extracting the start time of the current
+            # track
+            end_time = curr.strip('\n ', ).partition(':')[2]
+
+            tracks[track_id] = (track_name, start_time, end_time)
+
+    # manage edge case for the last line in the file
+    substr = curr.strip('\n ', )
+    track_id = substr.partition('.')[0]
+    track_name = substr[substr.find('.') + 1:substr.find(':')]
+    start_time = substr.partition(':')[2]
+
+    # get file length
+    tot_duration = subprocess.run(["ffprobe", "-i", 'bdsp.flac', "-show_entries",
+                                   "format=duration", "-v", "quiet", "-of", "csv=p=0",
+                                   "-sexagesimal"], cwd=f'{SOURCE_PATH}', capture_output=True,
+                                  encoding='UTF-8').stdout[:7]
+
+    # add final track to dictionary
+    tracks[track_id] = (track_name, start_time, tot_duration)
+
+    # check that the file exists.If not, create the directory.
+    file = f'{OUTPUT_PATH}/out.txt'
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+
+    # if the file exists, overwrite it
+    with open(file, 'w') as filetowrite:
+        filetowrite.write(' ------------------------------------\n')
+        filetowrite.write(f'| Track | Name | startTime - endTime |\n')
+        filetowrite.write(' ------------------------------------\n')
+
+        for key, val in tracks.items():
+            filetowrite.write(f'|{key}|{val[0]}|{val[1]}-{val[2]}|\n')
+
+    return tracks
--- split_audio.py
+++ split_audio.py
@@ -1,4 +1,5 @@
 import subprocess
+from audio_times_parser import *
 
 """Audio trimmer (pokemon + rain sounds)
 
@@ -9,6 +10,7 @@ import subprocess
 5. mix each one with rain, take the shortest
 
 """
+
 
 # internal class to print colors to the terminal
 class bcolors:
@@ -22,37 +24,54 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# constants 
-SOURCE_PATH = '/Users/renegade/MyRepos/automated-audio-splitter/'
-DESTINATION_PATH = '/Users/renegade/MyRepos/automated-audio-splitter/BDSP/'
-
 
 if __name__ == "__main__":
-        
-    subprocess.run(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", "01:49:42", "-to", "01:52:40", "-i", f'bdsp.flac',  f'{DESTINATION_PATH}/Mt_Coronet.flac'],cwd=f'{SOURCE_PATH}',check=True)
+
+    # obtains all information from tracks
+    tracks = parseTrackList()
+
+    if not os.path.exists(f'{DESTINATION_PATH}'):
+        os.makedirs(f'{DESTINATION_PATH}')
+
+    if not os.path.exists(f'{OUT}'):
+        os.makedirs(f'{DESTINATION_PATH}')    
+
+    for key, val in tracks.items():
+        name = tracks[f'{key}'][0]
+        start = tracks[f'{key}'][1]
+        end = tracks[f'{key}'][2]
+
+        subprocess.run(
+            ["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", f'{start}', "-to", f'{end}',
+             "-i", f'{ORIGIN_TRACK}', f'{key}.{DESTINATION_PATH}/{name}.flac'], cwd=f'{SOURCE_PATH}',
+            check=True)
+
+        subprocess.run(["ffmpeg", "-hide_banner", "-loglevel", "error", "-i",
+                        f'{DESTINATION_PATH}/{name}.flac',
+                        "-i", "rain.flac", "-filter_complex",
+                        "amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,"
+                        "volume=2", f'{name}.flac'], cwd=f'{SOURCE_PATH}', check=True)
+        print(bcolors.OKCYAN + f'{name}' + bcolors.ENDC)
+
+
+
+
+    """        run(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", "01:49:42", "-to", "01:52:40", "-i", "bdsp.flac",  f'{DESTINATION_PATH}/Mt_Coronet.flac'],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Lake theme" + bcolors.ENDC)
 
-    subprocess.run(["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "BDSP/Mt_Coronet.flac", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Mt_Coronet.flac"],cwd=f'{SOURCE_PATH}',check=True)
+    run(["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "desriantion_path", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Mt_Coronet.flac"],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Lake theme + RAIN" + bcolors.ENDC)
 
-    subprocess.run(["ffmpeg","-hide_banner", "-loglevel", "error", "-ss", "02:54:05", "-to", "02:57:38", "-i", "bdsp.flac",  f'{DESTINATION_PATH}/Old_Chateau.flac'],cwd=f'{SOURCE_PATH}',check=True)
+    run(["ffmpeg","-hide_banner", "-loglevel", "error", "-ss", "02:54:05", "-to", "02:57:38", "-i", "bdsp.flac",  f'{DESTINATION_PATH}/Old_Chateau.flac'],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Old Chateau" + bcolors.ENDC)
 
-    subprocess.run(["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "BDSP/Old_Chateau.flac", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Old_Chateau.flac"],cwd=f'{SOURCE_PATH}',check=True)
+    run(["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "BDSP/Old_Chateau.flac", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Old_Chateau.flac"],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Old_Chateau + RAIN" + bcolors.ENDC)
 
-    subprocess.run(["ffmpeg","-hide_banner", "-loglevel", "error", "-ss", "02:18:56", "-to", "02:21:51", "-i", "bdsp.flac",  f'{DESTINATION_PATH}/Pokemon_Center_Night.flac'],cwd=f'{SOURCE_PATH}',check=True)
+    run(["ffmpeg","-hide_banner", "-loglevel", "error", "-ss", "02:18:56", "-to", "02:21:51", "-i", "bdsp.flac",  f'{DESTINATION_PATH}/Pokemon_Center_Night.flac'],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Pokemon Center (Night)" + bcolors.ENDC)
 
-    subprocess.run(["ffmpeg", "-i", "BDSP/Pokemon_Center_Night.flac", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Pokemon_Center_Night.flac"],cwd=f'{SOURCE_PATH}',check=True)
+    run(["ffmpeg", "-i", "BDSP/Pokemon_Center_Night.flac", "-i", "rain.flac", "-filter_complex" ,"amix=inputs=2:duration=first:weights='1 1.9':dropout_transition=0,volume=2", "Pokemon_Center_Night.flac"],cwd=f'{SOURCE_PATH}',check=True)
     print(bcolors.OKCYAN + "Pokemon_Center (Night) + RAIN" + bcolors.ENDC)
-
-
-
-
-
-
-
-
-
+"""
 
